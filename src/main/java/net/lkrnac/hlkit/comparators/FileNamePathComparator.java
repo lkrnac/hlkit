5aed1ac320b982b68d4d0b99228bed5e49542b1f
lkrnac <lubos.krnac@gmail.com>
2014-03-09 09:12:25 +0000

    Forgotten Javadoc added

--- src/main/java/net/lkrnac/hlkit/comparators/FileNamePathComparator.java
+++ src/main/java/net/lkrnac/hlkit/comparators/FileNamePathComparator.java
@@ -3,17 +3,49 @@ package net.lkrnac.hlkit.comparators;
 import java.nio.file.Path;
 import java.util.Comparator;
 
+/**
+ * Comparator for {@link Path} based on file name.
+ * 
+ * @author lubos krnac
+ * 
+ */
 public class FileNamePathComparator implements Comparator<Path> {
 	private boolean caseSensitive;
 
+	/**
+	 * Creates {@link FileNamePathComparator} instance. Comparison of file names
+	 * will be case sensitive.
+	 */
 	public FileNamePathComparator() {
 		this(true);
 	}
 
+	/**
+	 * Creates {@link FileNamePathComparator} instance with given case
+	 * sensitivity.
+	 * 
+	 * @param caseSensitive
+	 *            flag if comparison should be case sensitive
+	 */
 	public FileNamePathComparator(boolean caseSensitive) {
 		this.caseSensitive = caseSensitive;
 	}
 
+	/**
+	 * Compares two {@link Path} objects based on file name.
+	 * 
+	 * <b>JavaDoc from parent:</b><br>
+	 * {@inheritDoc}
+	 * 
+	 * @param path1
+	 *            path object 1 to compare
+	 * @param path2
+	 *            path object 2 to compare
+	 * @return comparison result according {@link Comparator} result contract
+	 * @throws NullPointerException
+	 *             if any of the comparing object is null or any file name is
+	 *             null
+	 */
 	@Override
 	public int compare(Path path1, Path path2) {
 		String fileName1 = path1.getFileName().toString();
